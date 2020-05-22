9566d35e3d34838502be853f4b90a32959c795e5
m0zart89 <mozart-freelance@yandex.ru>
2020-05-22 21:44:19 +0100

    changed app test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Beees!!!!
+            Beees!!!!@@@@
         </p>
         <a
           className="App-link"
